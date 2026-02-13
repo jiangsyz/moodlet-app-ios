63fff26bdce9b700350a8212af7ca3c1e3695815
jiangsyz <305460883@qq.com>
2026-02-13 20:47:50 +0800

    Initial Commit

---
+++ moodlet-app-ios/moodlet_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodlet_app_iosApp.swift
+//  moodlet-app-ios
+//
+//  Created by 蒋深 on 2026/2/13.
+//
+
+import SwiftUI
+
+@main
+struct moodlet_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ moodlet-app-iosTests/moodlet_app_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  moodlet_app_iosTests.swift
+//  moodlet-app-iosTests
+//
+//  Created by 蒋深 on 2026/2/13.
+//
+
+import Testing
+@testable import moodlet_app_ios
+
+struct moodlet_app_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
